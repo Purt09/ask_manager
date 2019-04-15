db38ce4bc8bc8eef8d1fbf0904e06e33c246294d
Alexandr Purtov <alexpurtv@gmail.com>
2019-04-15 05:23:53 +0300

    'set_up_route_user_task'

---
+++ modules/task/controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\modules\task\controllers;
+
+use app\modules\task\models\Task;
+use yii\web\Controller;
+use Yii;
+
+class UserController extends Controller
+{
+    public function actionIndex(){
+
+        $modelsactive = Task::find()->where(['status' => 1])->all();
+        $modelsbad = Task::find()->where(['status' => 2])->all();
+
+
+        return $this->render('index', [
+            'modelsactive' => $modelsactive,
+            'modelspros' => $modelsbad
+        ]);
+    }
+}
