a122597318c0863f6be7ebd627b627c0ad69c9ee
Alexandr Purtov <alexpurtv@gmail.com>
2019-06-27 10:25:47 +0300

    'Add_vk_auth_16'

--- modules/user/models/AuthVk.php
+++ modules/user/models/AuthVk.php
@@ -73,46 +73,6 @@ class AuthVk
     }
 
 
-    public function get_user() {
-        if(!$this->token) {
-            exit('Wrong code');
-        }
-
-        if(!$this->uid) {
-            exit('Wrong code');
-        }
-        var_dump($this->token);
-        var_dump($this->uid);
-
-        $query = "user_ids=".$this->uid."&fields=first_name,last_name,nickname,screen_name,sex,bdate,city, country,timezone,photo,photo_medium,photo_big,has_mobile,rate,contacts, education,online,counters&access_token=".$this->token."&v=5.95";
-//echo $query;
-
-        $kur = curl_init();
-
-
-        var_dump($kur);
-        curl_setopt($kur, CURLOPT_URL, self::URL_GET_USER."?".$query);
-
-        var_dump(self::URL_GET_USER."?".$query);
-
-        curl_setopt($kur, CURLOPT_SSL_VERIFYPEER, false);
-
-        curl_setopt($kur, CURLOPT_SSL_VERIFYHOST, false);
-
-        curl_setopt($kur,CURLOPT_RETURNTRANSFER,TRUE);
-
-
-
-        $result2 = curl_exec($kur);
-
-        curl_close($kur);
-
-        $_SESSION['user'] = json_decode($result2);
-
-        $this->redirect("http://avtomirock.esy.es");
-
-    }
-
     public function getUser() {
         if(!$this->token) {
             exit('Wrong code');
@@ -122,7 +82,7 @@ class AuthVk
             exit('Wrong code');
         }
 
-        $query = "uids=".$this->uid."&fields=first_name,last_name,nickname,photo,photo_medium,photo_big&access_token=".$this->token;
+        $query = "user_ids=".$this->uid."&fields=first_name,last_name,nickname,photo,photo_medium,photo_big&access_token=".$this->token."&v=5.95";
 //echo $query;
 
         $kur = curl_init();
