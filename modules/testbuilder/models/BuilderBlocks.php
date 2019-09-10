8975e0051864294955062e8486293c60e5e02a6a
Alexandr Purtov <alexpurtv@gmail.com>
2019-09-10 06:28:15 +0300

    Ошибка

--- modules/testbuilder/models/BuilderBlocks.php
+++ modules/testbuilder/models/BuilderBlocks.php
@@ -36,7 +36,7 @@ class BuilderBlocks extends \yii\db\ActiveRecord
      */
     public static function tableName()
     {
-        return 'keys_builder_blocks';
+        return '{{%builder_blocks}}';
     }
 
     /**
