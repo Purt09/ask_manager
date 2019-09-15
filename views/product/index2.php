50b11f0336f9d981729c8b71931d684a6ed57daa
Alexandr Purtov <alexpurtv@gmail.com>
2019-09-15 10:55:48 +0300

    Лишнее удалено

--- views/product/index2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$this->registerMetaTag(['keywords' => Yii::t('app', 'Products').' '.yii::$app->params['mainTitle']]);
-$this->registerMetaTag(['description' => Yii::t('app', 'Products').' '.yii::$app->params['mainTitle']]);
-echo app\widgets\product\WidgetProductItems::widget([
-        'optionNew' => [
-            'pageSize' => 0,
-        ]
-    ]);
-?>
